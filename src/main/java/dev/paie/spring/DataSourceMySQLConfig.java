2ce280ea3ce9ed69ef3280f83492ce00f7066184
VanRottanaYOU <vr.you@hotmail.fr>
2017-09-05 12:12:13 +0200

    TP ajout JDBC grade

--- src/main/java/dev/paie/spring/DataSourceMySQLConfig.java
+++ src/main/java/dev/paie/spring/DataSourceMySQLConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+
 @Configuration
 @ComponentScan({"dev.paie.service", "dev.paie.util"})
 @ImportResource("classpath:jdd-config.xml")
