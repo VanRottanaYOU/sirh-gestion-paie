7b942d773a438ffd0d56a74f10367bcde946e081
VanRottanaYOU <vr.you@hotmail.fr>
2017-09-05 15:47:37 +0200

    Ajout TP JPA

--- src/main/java/dev/paie/service/CotisationServiceJpa.java
+++ src/main/java/dev/paie/service/CotisationServiceJpa.java
@@ -8,7 +8,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.paie.entite.Cotisation;
 
